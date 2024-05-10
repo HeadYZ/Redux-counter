7e92ce4c76195452b81c79f186109858b46af439
Patryk <krasowski.p@outlook.com>
2024-05-10 20:38:33 +0200

    add inrecement & decrement fn, add hooks

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,29 @@
-import classes from './Counter.module.css';
+import { useSelector, useDispatch } from 'react-redux'
+import classes from './Counter.module.css'
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+	const dispatch = useDispatch()
+	const counter = useSelector(state => state.counter)
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
-};
+	const incrementHandler = () => {
+		dispatch({ type: 'INCREMENT' })
+	}
+	const decrementHandler = () => {
+		dispatch({ type: 'DECREMENT' })
+	}
 
-export default Counter;
+	const toggleCounterHandler = () => {}
+
+	return (
+		<main className={classes.counter}>
+			<h1>Redux Counter</h1>
+			<div className={classes.value}>{counter}</div>
+			<button onClick={incrementHandler}>Increment</button>
+			<button onClick={decrementHandler}>Decrement</button>
+			<div></div>
+			<button onClick={toggleCounterHandler}>Toggle Counter</button>
+		</main>
+	)
+}
+
+export default Counter
